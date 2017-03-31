eec373fd8a3dc1b78d9fd7a78177208c6f683a87
Stephen Boniface <stevieray8450@gmail.com>
2017-03-30 22:03:56 -0500

    first commit

---
+++ TestAppXamarin/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestAppXamarin
+{
+	class Employee
+	{
+		public int ID;
+		public string Name;
+		public Gender gender;
+		public int Experience;
+
+		public override string ToString()
+		{
+			return string.Format(this.Name, this.ID, this.gender, this.Experience);
+		}
+	}
+
+	public enum Gender
+	{
+		Unknown,
+		Male,
+		Female
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Employee emp1 = new Employee() { ID = 101, Experience = 5, Name = "Steve", gender = Gender.Male };
+			Console.WriteLine(emp1.gender.ToString());
+			emp1.ToString();
+		}
+	}
+}
